a3c2284b62a1c832d3549cf7edaf989a77c4f263
Bera <bera.koc@getir.com>
2022-05-27 11:33:54 +0300

    Removed unnecessary methods

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,4 @@
-import { isNumber, pick } from 'lodash';
+import { pick } from 'lodash';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
