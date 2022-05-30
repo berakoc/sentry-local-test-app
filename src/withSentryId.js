e7c9ffeda6f2078e4978f633cba4392dadb7e3b9
Bera <bera.koc@getir.com>
2022-05-30 10:18:32 +0300

    Fixed null component name

--- src/withSentryId.js
+++ src/withSentryId.js
@@ -1,7 +1,11 @@
 import createSentryDataIdGenerator from './createSentryDataId';
 
+function getDisplayName(Component) {
+    return Component.displayName || Component.name || 'Component';
+}
+
 export default function withSentryId(Component) {
-    
-    const generateSentryId = createSentryDataIdGenerator(Component.name);
+    const displayName = getDisplayName(Component);
+    const generateSentryId = createSentryDataIdGenerator(displayName);
     return (props) => <Component {...props} generateSentryId={generateSentryId} />;
 }
